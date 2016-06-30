3297b7c3682037f984dbbb260d15d0164a016018
Felix Arntz <felix-arntz@t-online.de>
2016-06-30 18:56:01 +0200

    update to match plugin changes

--- templates/element-type-multiplechoice.php
+++ templates/element-type-multiplechoice.php
@@ -2,7 +2,7 @@
 /**
  * Template: element-type-multiplechoice.php
  *
- * Available data: $element_id, $type, $id, $name, $classes, $description, $required, $answers, $response, $extra_attr, $limits_text
+ * Available data: $element_id, $type, $id, $name, $classes, $description, $required, $answers, $response, $has_error, $has_success, $extra_attr, $limits_text
  *
  * @package TFBS3
  * @subpackage Templates
@@ -10,12 +10,15 @@
  * @since 1.0.0
  */
 
+$aria_describedby = ' aria-describedby="' . esc_attr( $id ) . '-description' . ( $has_error ? ' ' . esc_attr( $id ) . '-errors' : '' ) . '"';
+$aria_invalid = $has_error ? ' aria-invalid="true"' : '';
+$aria_required = $required ? ' aria-required="true"' : '';
 $wrap_class = torro()->extensions()->get_registered( 'bootstrap_for_torro_forms' )->is_group_inline() ? 'checkbox-inline' : 'checkbox';
 ?>
 <?php foreach ( $answers as $i => $answer ) : ?>
 	<div class="torro_element_checkbox <?php echo $wrap_class; ?>">
 		<?php $checked = is_array( $response ) && in_array( $answer['value'], $response, true ) ? ' checked' : ''; ?>
-		<input type="checkbox" id="<?php echo esc_attr( $id ) . '-' . $i; ?>" name="<?php echo esc_attr( $name ); ?>[]" class="<?php echo esc_attr( implode( ' ', $classes ) ); ?>" value="<?php echo esc_attr( $answer['value'] ); ?>"<?php echo $checked; ?> aria-describedby="<?php echo esc_attr( $id ); ?>-description <?php echo esc_attr( $id ); ?>-errors">
+		<input type="checkbox" id="<?php echo esc_attr( $id ) . '-' . $i; ?>" name="<?php echo esc_attr( $name ); ?>[]" class="<?php echo esc_attr( implode( ' ', $classes ) ); ?>" value="<?php echo esc_attr( $answer['value'] ); ?>"<?php echo $checked . $aria_describedby; ?>>
 		<label for="<?php echo esc_attr( $id ) . '-' . $i; ?>">
 			<?php echo esc_html( $answer['label'] ); ?>
 		</label>
--- templates/element-type-onechoice.php
+++ templates/element-type-onechoice.php
@@ -2,7 +2,7 @@
 /**
  * Template: element-type-onechoice.php
  *
- * Available data: $element_id, $type, $id, $name, $classes, $description, $required, $answers, $response, $extra_attr
+ * Available data: $element_id, $type, $id, $name, $classes, $description, $required, $answers, $response, $has_error, $has_success, $extra_attr
  *
  * @package TFBS3
  * @subpackage Templates
@@ -10,12 +10,15 @@
  * @since 1.0.0
  */
 
+$aria_describedby = ' aria-describedby="' . esc_attr( $id ) . '-description' . ( $has_error ? ' ' . esc_attr( $id ) . '-errors' : '' ) . '"';
+$aria_invalid = $has_error ? ' aria-invalid="true"' : '';
+$aria_required = $required ? ' aria-required="true"' : '';
 $wrap_class = torro()->extensions()->get_registered( 'bootstrap_for_torro_forms' )->is_group_inline() ? 'radio-inline' : 'radio';
 ?>
 <?php foreach ( $answers as $i => $answer ) : ?>
 	<div class="torro_element_radio <?php echo $wrap_class; ?>">
 		<?php $checked = $answer['value'] === $response ? ' checked' : ''; ?>
-		<input type="radio" id="<?php echo esc_attr( $id ) . '-' . $i; ?>" name="<?php echo esc_attr( $name ); ?>" class="<?php echo esc_attr( implode( ' ', $classes ) ); ?>" value="<?php echo esc_attr( $answer['value'] ); ?>"<?php echo $checked; ?> aria-describedby="<?php echo esc_attr( $id ); ?>-description <?php echo esc_attr( $id ); ?>-errors">
+		<input type="radio" id="<?php echo esc_attr( $id ) . '-' . $i; ?>" name="<?php echo esc_attr( $name ); ?>" class="<?php echo esc_attr( implode( ' ', $classes ) ); ?>" value="<?php echo esc_attr( $answer['value'] ); ?>"<?php echo $checked . $aria_describedby; ?>>
 		<label for="<?php echo esc_attr( $id ) . '-' . $i; ?>">
 			<?php echo esc_html( $answer['label'] ); ?>
 		</label>
